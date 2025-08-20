3831c6985bd4a02f8c321a64eb32a3bb369e8798
katerina <mel.katya@mail.ru>
2025-08-20 20:15:24 +0300

    add database config

--- fastapi_application/core/config.py
+++ fastapi_application/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,8 +7,30 @@ class RunConfig(BaseModel):
     port: int = 8000
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
+    db: DatabaseConfig
 
 
 settings = Settings()
