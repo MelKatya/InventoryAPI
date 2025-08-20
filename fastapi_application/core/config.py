952fa6c834df14df4220920ca1500d196c25d204
katerina <mel.katya@mail.ru>
2025-08-20 22:10:29 +0300

    add typing to naming_convention

--- fastapi_application/core/config.py
+++ fastapi_application/core/config.py
@@ -13,7 +13,7 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
-    naming_convention = {
+    naming_convention: dict[str, str] = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
