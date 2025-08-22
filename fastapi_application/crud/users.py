f918d48f4d7fcd05bbbf0f97788d122c791266f5
katerina <mel.katya@mail.ru>
2025-08-22 22:18:26 +0300

    add get all users

--- fastapi_application/crud/users.py
+++ fastapi_application/crud/users.py
@@ -1,4 +1,6 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from core.models import User, UserRole
 from core.schemas.user import UserCreate
@@ -8,12 +10,27 @@ async def create_user(session: AsyncSession, new_user: UserCreate):
     user = User(**new_user.model_dump(exclude={"role_id"}))
     session.add(user)
     await session.flush()
-    await add_role_to_user(session=session, user_id=user.id, roles_id=new_user.role_id)
+    await add_role_to_user(
+        session=session,
+        user_id=user.id,
+        roles_id=new_user.role_id,
+    )
     await session.commit()
     return user
 
 
-async def add_role_to_user(session: AsyncSession, user_id: int, roles_id: list[int]):
+async def add_role_to_user(
+        session: AsyncSession,
+        user_id: int,
+        roles_id: list[int],
+):
     new_some = [UserRole(user_id=user_id, role_id=role) for role in roles_id]
     session.add_all(new_some)
     await session.commit()
+
+
+async def get_all_users(session: AsyncSession):
+    stmt = select(User).options(selectinload(User.users_roles).selectinload(UserRole.roles)).order_by(User.id)
+    result = await session.scalars(stmt)
+    return result.all()
+
