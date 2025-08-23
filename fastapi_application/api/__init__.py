47c71087d3df5c54cd754552a896f9dfd4b949e2
katerina <mel.katya@mail.ru>
2025-08-23 22:10:17 +0300

    create auth route

--- fastapi_application/api/__init__.py
+++ fastapi_application/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .users import router as users_route
+from .authentication import router as auth_route
 
 router = APIRouter()
 
 router.include_router(users_route, prefix="/users")
+router.include_router(auth_route, prefix="/auth")
