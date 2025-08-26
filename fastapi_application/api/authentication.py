82f15341bc7612ec6f76334b4e25a207841b8c4d
katerina <mel.katya@mail.ru>
2025-08-26 19:26:17 +0300

    add logout router

--- fastapi_application/api/authentication.py
+++ fastapi_application/api/authentication.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Form, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.validate import validate_token
+from api.validation import validate_access_token, validate_roles, validate_refresh_token
 from core.models import db_helper, User
 from core.schemas.token import TokenInfo
 from crud import users as crud_us
-from crud.users import get_user_roles
+from crud.users import get_user_roles, get_user_by_id
 from security.hashed_utils import validate_password
 from security.jwt_create import create_access_token, create_refresh_token
 
@@ -46,4 +46,29 @@ async def auth_user_jwt(
 
     return TokenInfo(access_token=access_token, refresh_token=refresh_token)
 
+
+@router.post("/logout")
+async def logout_user(
+    response: Response,
+    refresh_token: dict = Depends(validate_refresh_token),
+    access_token: dict = Depends(validate_access_token),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    response.delete_cookie("refresh_token")
+    user_id = int(access_token.get("sub"))
+    user = await get_user_by_id(session, user_id)
+    return {"Bye": user.username}
+
+
+@router.post("/refresh")
+async def auth_refresh_jwt(
+    token: dict = Depends(validate_access_token),
+):
+    print(token)
+    return {"token": "sks"}
+
+
+@router.get("/me")
+async def auth_user_check_self_info():
+    ...
 
