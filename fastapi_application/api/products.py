8112d5bc857e74cdfad3d8f1acc8b4173ca1d76a
katerina <mel.katya@mail.ru>
2025-08-28 16:48:23 +0300

    add patch product

--- fastapi_application/api/products.py
+++ fastapi_application/api/products.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.dependency.product import product_by_id
 from api.validation import validate_roles, validate_access_token
 from core.models import db_helper, Product
-from core.schemas.product import ProductCreate, ProductBase
+from core.schemas.product import ProductCreate, ProductBase, ProductUpdatePartial
 from crud import product as prod
 
 router = APIRouter(tags=["Products"])
@@ -69,3 +69,27 @@ async def update_product(
 
     product_update = ProductCreate(**product_update.model_dump(), supplier_id=creator_id)
     return await prod.update_product(session, product, product_update)
+
+
+@router.patch("/{product_id}")
+@validate_roles({"admin", "supplier"})
+async def update_product_partial(
+    product_update: ProductUpdatePartial,
+    product: Product = Depends(product_by_id),
+    session: AsyncSession = Depends(db_helper.session_getter),
+    token: dict = Depends(validate_access_token),
+):
+    creator_id = product.supplier_id
+    supplier_id = int(token.get("sub"))
+    roles = token.get("roles")
+
+    if creator_id != supplier_id or "admin" not in roles:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access to modify the product is denied"
+        )
+
+    product_update = ProductCreate(**product_update.model_dump(), supplier_id=creator_id)
+    return await prod.update_product(session, product, product_update, partial=True)
+
+
