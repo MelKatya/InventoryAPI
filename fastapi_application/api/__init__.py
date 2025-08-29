dc7e78f521be641c366a7be0395d2500d4277cb1
katerina <mel.katya@mail.ru>
2025-08-29 16:05:27 +0300

    add orders router

--- fastapi_application/api/__init__.py
+++ fastapi_application/api/__init__.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 from .users import router as users_route
 from .authentication import router as auth_route
 from .products import router as product_route
+from .orders import router as order_route
 
 router = APIRouter()
 
 router.include_router(users_route, prefix="/users")
 router.include_router(auth_route, prefix="/auth")
 router.include_router(product_route, prefix="/products")
-
+router.include_router(order_route, prefix="/orders")
