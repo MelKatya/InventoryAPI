7c9a8d9cca343299448e90a9bab6433c98060d7b
katerina <mel.katya@mail.ru>
2025-08-29 16:04:17 +0300

    add update order's status

--- fastapi_application/api/orders.py
+++ fastapi_application/api/orders.py
@@ -32,4 +32,15 @@ async def get_all_orders(
     return await ord.get_all_orders(session=session, customer_id=int(payload.get("sub")))
 
 
+@router.patch("/{order_id}/update_status")
+@validate_roles({"admin", "customer"})
+async def update_status(
+    new_status: Annotated[str, Literal["new", "confirmed", "cancelled"]],
+    session: AsyncSession = Depends(db_helper.session_getter),
+    payload: dict = Depends(validate_access_token),
+    order: Order = Depends(order_by_id),
+):
+    creator_id = order.customer_id
+    check_creator(creator_id, payload, "order")
+    return await ord.update_order_status(session=session, order=order, new_status=new_status)
 
